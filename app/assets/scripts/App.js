08a5f7b21091d24ea8def3c50bc35de7f079dff9
Jacob <turner_jacob@hotmail.com>
2017-10-24 15:31:41 -0500

    Completed reveal on scroll class selector

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
-
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import Modal from './modules/Modal';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
 var modal = new Modal();
+new RevealOnScroll($(".future__content-container"), "70%");
